d7cfca8ae9806ad687107a49c10ac1e0cdad2bad
Luis <luisbarrios0110@gmail.com>
2025-08-30 22:39:32 -0500

    feat: añadiendo preact para el componente dinamico

---
+++ src/components/Inventario/Inventary.tsx
@@ -0,0 +1,26 @@
+import { materiales } from '@/data/materiales'
+
+export default function Inventary () {
+  type Material = typeof materiales[0];
+  const materialesClave = Object.keys(materiales[0]) as Array<keyof Material>;
+
+  return (
+    <table className={'inventarioTable'}>
+      {/* <caption>Materiales en Inventario</caption>  */} 
+      <thead>
+        <tr>
+          {materialesClave.map( clave => <th>{clave.toLocaleUpperCase()}</th>)}
+          <th>Opcion</th>
+        </tr>
+      </thead>
+      <tbody>
+        {materiales.map( material => (
+          <tr>
+            {materialesClave.map( clave => (<td style={clave == 'cantidad' || clave == 'nombre'  ? 'font-weight:bold;text-align:center;' : ''} >  {material[clave]}</td>))}
+          </tr>
+        ))}
+      </tbody>
+       {/* <tfoot>Tabla de Materiales en el sistema</tfoot>   */}
+    </table>
+  )
+}
