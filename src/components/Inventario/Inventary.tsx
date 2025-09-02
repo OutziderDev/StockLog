7d002a1903b07bca6a81b5273828c996f624a097
Luis <luisbarrios0110@gmail.com>
2025-09-01 21:55:04 -0500

    feat: migrando los filtros al componente reactivo

--- src/components/Inventario/Inventary.tsx
+++ src/components/Inventario/Inventary.tsx
@@ -1,11 +1,39 @@
 import { materiales } from '@/data/materiales'
 import '@/components/Inventario/inventary.css'
+import LupaIcon from '@/assets/icons/lupa.svg'
 
 export default function Inventary () {
   type Material = typeof materiales[0];
   const materialesClave = Object.keys(materiales[0]) as Array<keyof Material>;
 
   return (
+    <>
+    <section>
+      <h2 class="sr-only">Panel de Filtros</h2>
+      <form action="">
+         
+        <label for="inputbusqueda" class="sr-only">Buscar Productos</label>
+        <input type="text" name="inputbusqueda" id="inputbusqueda" class="inputbase" placeholder="Buscar Producto..." />
+
+        <label for="inputCategoria" class="sr-only">Busqueda con Categoría</label>
+        <select name="inputCategoria" class="inputbase" id="inputCategoria">
+          <option value="" disabled selected>Selecciona una categoría</option>
+          <option value="herramientas">Herramientas</option>
+          <option value="materiales">Materiales</option>
+          <option value="maquinaria">Maquinaria</option>
+        </select>
+
+        <label for="inputUbicacion" class="sr-only">Busqueda con Bodega</label>
+        <select name="inputUbicacion" class="inputbase" id="inputUbicacion">
+          <option value="" disabled selected>Selecciona una Ubicacíon</option>
+          <option value="bodega1">Bodega 1</option>
+          <option value="bodega2">Bodega 2</option>
+          <option value="bodega3">Bodega 3</option>
+        </select>
+        
+        <button type="submit"> <img src={LupaIcon.src} aria-hidden="true" /> Filtrar</button>
+      </form>
+    </section> 
     <table className={'inventarioTable'}>
       {/* <caption>Materiales en Inventario</caption>  */} 
       <thead>
@@ -23,5 +51,6 @@ export default function Inventary () {
       </tbody>
        {/* <tfoot>Tabla de Materiales en el sistema</tfoot>   */}
     </table>
+    </>  
   )
 }
