92cf4f5767a200cc352eda8a1bd028b658c5d437
Luis <luisbarrios0110@gmail.com>
2025-09-10 18:21:27 -0500

    feat: crear endpoint para obtener los reportes

--- src/pages/api/reportes.ts
+++ src/pages/api/reportes.ts
@@ -1,19 +1,31 @@
 import type { APIRoute } from "astro";
 import { supabase } from '@/scripts/supabaseClient'
 
-/* export const GET: APIRoute = async () => {
+ export const GET : APIRoute = async () => {
   try {
-    return new Response (
-      JSON.stringify({saludo: "holaaa desde reportes"}),
-      {
-        status: 200,
-        headers: {
-          "Content-Type": "application/json"
+    const {data, error} = await supabase.from('facturas').select('id,cliente:clientes(nombre),tipo,total,fecha')
+
+    if (error) {
+      return new Response(
+        JSON.stringify({ mensaje: 'algo va mal en query de facturas', error }),
+        {
+          status: 500,
+          headers: { 'Content-type': 'application/json' }
         }
-      }
-    )
-  } catch (error) {
-    console.error('problema',error);
+      );
+    }
+    const formattedWithCliente = data.map( ( reporte : any) => ({
+      ...reporte,
+      cliente: reporte.cliente.nombre 
+    }))
+
+    return new Response( JSON.stringify(formattedWithCliente), {
+      status:200,
+      headers: { 'Content-type': 'application/json'}
+    })
+
     
+  } catch (error) {
+    return new Response( JSON.stringify({mensaje: 'algo va mal', error}))
   }
-} */
+} 
