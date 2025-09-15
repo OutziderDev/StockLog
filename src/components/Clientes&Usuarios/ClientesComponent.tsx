5988e637d8cbd2210e752a937918266fc15f6569
Luis <luisbarrios0110@gmail.com>
2025-09-14 20:39:10 -0500

    feat: añadir componente ClientesComponent para gestionar filtros y tabla de clientes en la página de clientes registrados

---
+++ src/components/Clientes&Usuarios/ClientesComponent.tsx
@@ -0,0 +1,11 @@
+import ClientesFiltros from "@/components/Clientes&Usuarios/ClientesFiltros"
+import ClientesTabla from "@/components/Clientes&Usuarios/ClientesTabla"
+
+export default function ClientesComponent() {
+  return (
+    <>
+      <ClientesFiltros />
+      <ClientesTabla />
+    </>
+  )
+}
