70561b0920f4f6360e84029784b02210fbc1e887
Luis <luisbarrios0110@gmail.com>
2025-09-18 16:42:05 -0500

    feat: cargando la tabla de usuarios con los datos del fetch y mapeando segun filtro

--- src/components/Clientes&Usuarios/Usuarios/UsuariosTabla.tsx
+++ src/components/Clientes&Usuarios/Usuarios/UsuariosTabla.tsx
@@ -5,9 +5,39 @@ interface UsuarioTablaProps {
 }
 const UsuariosTabla = ({dataUsuarios}: UsuarioTablaProps) => {
   console.log('data user:',dataUsuarios);
+  const usuarioExacto = dataUsuarios
   
   return(
-    <div>tabla</div>
+    <table className={"tablestyles"}>
+      <thead>
+        <tr>
+          <th>id</th>
+          <th>nombre</th>
+          <th>opción</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          usuarioExacto.length >= 1 ? 
+          ( 
+            usuarioExacto.map( usuario => (
+              <tr>
+                <td>{usuario.id}</td>
+                <td>{usuario.nombre}</td>
+                <td>view</td>
+              </tr>
+            ))
+          )  :
+          (
+            <tr>
+              <td>─</td>
+              <td>Sin Coincidencias</td>
+              <td>─</td>
+            </tr>
+          )
+        }
+      </tbody>
+    </table>
   )
 }
 
