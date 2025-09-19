a8da65626dc35e1cd0230cb735ee0f08a099ba27
Luis <luisbarrios0110@gmail.com>
2025-09-19 16:10:38 -0500

    feat: agregando el endpoint para crear nuevos clientes

--- src/pages/api/clientes.ts
+++ src/pages/api/clientes.ts
@@ -19,3 +19,26 @@ export const GET : APIRoute = async () => {
     return new Response("paso algo", {status: 500})
   }
 }
+
+export const POST : APIRoute = async ({ request }) => {
+  try {
+    const { nombre, email, direccion } = await request.json()
+    
+    if (!nombre || !email || !direccion ) {
+      return new Response( JSON.stringify({error: "Faltan campos obligatorios"}) ,{status:400})
+    }
+
+    const { data, error } = await supabase
+    .from('clientes')
+    .insert({ nombre, email,direccion })
+  
+    if(error){
+      return new Response( JSON.stringify({error: error.message}), {status:500})
+    }
+
+    return new Response( JSON.stringify(data), {status:201, headers: {"Content-type": "application/json"}})
+    
+  } catch (error) {
+    return new Response(JSON.stringify({ error: "Fallo en la petición"}), {status: 500})
+  }
+}
