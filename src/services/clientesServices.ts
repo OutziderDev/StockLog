83c6757ea2653fa65012001b2a870a6d5f937f5d
Luis <luisbarrios0110@gmail.com>
2025-09-19 16:38:28 -0500

    feat: crear el servicio para agregar nuevos clientes con una funchion asyc fetcher

--- src/services/clientesServices.ts
+++ src/services/clientesServices.ts
@@ -1,6 +1,17 @@
 import { baseURL } from "./utility";
+import type { Cliente } from "@/types/clientesTypes";
 
 export const getClientes = async () => {
   const respuesta = await fetch(`${baseURL}/clientes`);
   return respuesta.json();
 } 
+
+export const crearCliente = async (nuevoCliente : Cliente) => {
+  const respuesta = await fetch(`${baseURL}/clientes`, 
+    { method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(nuevoCliente)
+    }
+  )
+  return respuesta.json()
+}
